e7c3a2aabeb320394f0b8f173f2f46c4e4ddab9e
ispan <ispan@DESKTOP-PAKSETB>
2022-11-09 14:01:12 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<int> items = new List<int> { 577, 54, 588, 12, 3, 8, 90, 11 };
+			Func<int, bool> isEven = n => n % 2 == 0;
+			List<int> EvenItems = GetEvenItems(items, isEven);
+			foreach (var item in EvenItems)
+			{
+				Console.WriteLine(item);
+			}
+		}
+		static List<int> GetEvenItems(List<int> source, Func<int, bool> func)
+		{
+			var result = new List<int>();
+
+			foreach (int item in source)
+			{
+				if (func(item) == true)
+				{
+					result.Add(item);
+				}
+			}
+
+			return result;
+		}
+	}
+
+}
